bf3a19d02700baef8260ba5b3dad292fc0066cec
Viktor Falkesjö <viktorfalkesjo@gmail.com>
2024-02-06 12:21:44 +0100

    annoteringarochentity

--- egetapidb/src/main/java/org/egetapidb/post/model/Post.java
+++ egetapidb/src/main/java/org/egetapidb/post/model/Post.java
@@ -1,5 +1,36 @@
 package org.egetapidb.post.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Post {
+
+    private String title;
+    private String text;
+    private Long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
--- egetapidb/src/main/java/org/egetapidb/user/model/User.java
+++ egetapidb/src/main/java/org/egetapidb/user/model/User.java
@@ -1,5 +1,33 @@
 package org.egetapidb.user.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private UUID apikey;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(UUID apikey) {
+        this.apikey = apikey;
+    }
 }
